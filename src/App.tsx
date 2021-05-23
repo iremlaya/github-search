417d0785d06f2923ce9fb59173d6a1052a94174a
iremlaya <iremlcaya@gmail.com>
2021-05-23 23:07:52 +0300

    routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
+import Results from "./components/pages/Results";
+import EmptyPage from "./components/pages/EmptyPage";
+import AppProvider from "./context/appContext";
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <div
+            style={{
+              paddingTop: "70px",
+              paddingLeft: "365px",
+              maxWidth: "calc(100%-360px)",
+            }}
+          >
+            <Switch>
+              <Route exact path="/" component={EmptyPage} />
+              <Route exact path="/results" component={Results} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </AppProvider>
   );
 }
 
